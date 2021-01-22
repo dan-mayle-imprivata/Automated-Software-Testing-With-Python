7f50792898e573cecc83a03c35c76bcdeb1489d4
SCDan0624 <dmayle1@hotmail.com>
2021-01-21 16:47:38 -0800

    completed variables refresher

---
+++ 01_variables/code.py
@@ -0,0 +1,28 @@
+# Variables Numbers
+x = 15
+
+price = 9.99
+
+discount = 0.2
+
+result = price * (1 - discount)
+
+print(result)  # 7.992
+
+# Variables Strings
+name = "Rolf"
+
+print(name)  # Rolf
+print(name * 2)  # RolfRolf
+
+# Changing Variable Values
+a = 25
+b = a
+
+print(a)  # 25
+print(b)  # 25
+
+b = 17
+
+print(a)  # 25
+print(b)  # 17
