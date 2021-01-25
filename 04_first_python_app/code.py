316cf22837266b5a9082c47033ffa23bf31abbf1
SCDan0624 <dmayle1@hotmail.com>
2021-01-25 13:17:14 -0800

    My first app lesson complete

---
+++ 04_first_python_app/code.py
@@ -0,0 +1,10 @@
+# Example 1
+user_age = input("Enter your age: ")
+years = int(user_age)
+months = years * 12
+print(f"Your age, {years}, is equal to {months} months.")
+
+# Example 2
+user_age2 = int(input("Enter your age: "))
+months2 = user_age2 * 12
+print(f"Your age, {user_age2}, is equal to {months2} months")
