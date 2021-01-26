5c17d2375c28b36cadb7e2344291127275c43239
SCDan0624 <dmayle1@hotmail.com>
2021-01-26 15:47:55 -0800

    in keyword lesson added

---
+++ 09_the_in_keyword/code.py
@@ -0,0 +1,7 @@
+friends = ["Dan", "Megan", "Jen"]
+print("Jen" in friends)  # True
+
+movies_watched = {"The Matrix", "Terminator", "Deadpool"}
+user_movie = input("Enter a movie you have seen recently: ")
+
+print(user_movie in movies_watched)
