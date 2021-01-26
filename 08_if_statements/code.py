45e23cbc7e4d6972dbfaf639010b61b52e541a85
SCDan0624 <dmayle1@hotmail.com>
2021-01-26 15:43:28 -0800

    Added if/else lesson

---
+++ 08_if_statements/code.py
@@ -0,0 +1,8 @@
+day_of_week = input("What day of the week is it today?").lower()
+
+if day_of_week == "monday":
+    print("Have a great start to your week!")
+elif day_of_week == "tuesday":
+    print("It is Tuesday")
+else:
+    print("Full speed ahead!")
