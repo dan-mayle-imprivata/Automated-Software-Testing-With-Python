4e46dea4325e8fd765a68cefb6386e4669535ce0
SCDan0624 <dmayle1@hotmail.com>
2021-01-29 11:54:15 -0800

    List comprehension completed

---
+++ 12_list_comprehension/code.py
@@ -0,0 +1,7 @@
+numbers = [1, 3, 5]
+doubled = [x * 2 for x in numbers]
+
+friends = ["Rolf", "Sam", "Samantha", "Saurabh", "Jen"]
+starts_s = [friend for friend in friends if friend.startswith("S")]
+
+print(starts_s)
