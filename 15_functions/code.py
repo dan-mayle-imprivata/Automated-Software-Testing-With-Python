964ce31381636edea1d31b27d198de8fbeda6212
SCDan0624 <dmayle1@hotmail.com>
2021-02-01 16:05:54 -0800

    functions lesson completed

---
+++ 15_functions/code.py
@@ -0,0 +1,16 @@
+def hello():
+    print("Hello!")
+
+
+hello()
+
+
+def user_age_in_seconds():
+    user_age = int(input("Enter your age: "))
+    age_seconds = user_age * 365 * 24 * 60 * 60
+    print(f"Your age in seconds is {age_seconds}.")
+
+
+print("Welcome to the age in seconds program!")
+user_age_in_seconds()
+print("Goodbye!")
