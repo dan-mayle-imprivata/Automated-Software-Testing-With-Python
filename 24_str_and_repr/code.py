5f51f6a37878238ab031d75526da28aa1682f8ea
SCDan0624 <dmayle1@hotmail.com>
2021-02-11 17:41:23 -0800

    str and repr lesson complete

---
+++ 24_str_and_repr/code.py
@@ -0,0 +1,19 @@
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def __str__(self):
+        return f"Person {self.name}, {self.age} years old"
+
+    def __repr__(self):
+        return f"<Person {self.name}, {self.age}>"
+
+
+bob = Person("Bob", 35)
+print(bob)
+# if using __str__
+# Person Bob, 35 years old
+
+# if using __repr__
+# <Person Bob, 35>
