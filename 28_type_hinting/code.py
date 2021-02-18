db6a27d6aea7c2f5e9b8a6a172ce5d7bc6461e5c
SCDan0624 <dmayle1@hotmail.com>
2021-02-18 14:59:58 -0800

    Type hint 3.5 lesson complete

---
+++ 28_type_hinting/code.py
@@ -0,0 +1,10 @@
+# from typing import List (Is how type hinting is commonly done)
+
+def list_average(sequence: List) -> float:
+    return sum(sequence) / len(sequence)
+
+
+list_average(123)
+
+
+# type hinting is a new feature for python 3.5
