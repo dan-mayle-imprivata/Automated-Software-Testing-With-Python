3ece0f23da1fd0043020be381fe5694508a90a82
SCDan0624 <dmayle1@hotmail.com>
2021-02-18 15:28:38 -0800

    Imports lesson completed

---
+++ 29_imports_in_python/code.py
@@ -0,0 +1,9 @@
+# import mymodule for entire page and not specific function
+# mymodule.divide would get you the function
+from mymodule import divide
+
+print(divide(10, 2))
+
+# prints the following
+# 5.0
+# mymodule.py:
---
+++ 29_imports_in_python/mymodule.py
@@ -0,0 +1,9 @@
+# Will write code here and then import to code.py
+
+
+def divide(dividend, divisor):
+    return dividend / divisor
+
+
+# __name__ Will allow you to tell which file you are running vs importing
+print("mymodule.py: ", __name__)  # mymodule.py:  __main__
