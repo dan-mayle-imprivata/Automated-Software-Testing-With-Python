35f73f1e04a92c7ee0494bf12d7b4c4a5bf23645
SCDan0624 <dmayle1@hotmail.com>
2021-02-22 11:11:45 -0800

    Decorator functions with params lesson complete

--- 36_decorator_functions_with_params/code.py
+++ 36_decorator_functions_with_params/code.py
@@ -5,18 +5,21 @@ user = {"username": "jose", "access_level": "admin"}
 
 def make_secure(func):
     @functools.wraps(func)
-    def secure_function():
+    def secure_function(*args, **kwargs):
         if user["access_level"] == "admin":
-            return func()
+            return func(*args, **kwargs)
         else:
             return f"No admin permissions for {user['username']}."
 
     return secure_function
 
 
-@make_secure  # will pass function through decorator in one go
-def get_admin_password():
-    return "1234"
+@make_secure
+def get_password(panel):
+    if panel == "admin":
+        return "1234"
+    elif panel == "billing":
+        return "super_secure_password"
 
 
-print(get_admin_password.__name__)  # get_admin_password
+print(get_password("billing"))
