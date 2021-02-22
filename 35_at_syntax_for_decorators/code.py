9536a93e73ceb528a0cc6ac7b7e5f9ccf48b1677
SCDan0624 <dmayle1@hotmail.com>
2021-02-22 11:05:28 -0800

    @ syntax with decorators lesson complete

---
+++ 35_at_syntax_for_decorators/code.py
@@ -0,0 +1,25 @@
+import functools
+
+user = {"username": "jose", "access_level": "admin"}
+
+
+def make_secure(func):
+    @functools.wraps(func)
+    def secure_function():
+        if user["access_level"] == "admin":
+            return func()
+        else:
+            return f"No admin permissions for {user['username']}."
+
+    return secure_function
+
+
+@make_secure  # will pass function through decorator in one go
+def get_admin_password():
+    return "1234"
+
+
+user = {"username": "jose", "access_level": "admin"}
+
+
+print(get_admin_password.__name__)
