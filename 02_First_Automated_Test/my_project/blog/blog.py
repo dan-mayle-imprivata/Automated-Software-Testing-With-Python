a5f275ce2bf7664592131bb7fbfe0f4ea0f81a06
SCDan0624 <dmayle1@hotmail.com>
2021-03-01 12:07:38 -0800

    __repr_method and TDD lesson complete

--- 02_First_Automated_Test/my_project/blog/blog.py
+++ 02_First_Automated_Test/my_project/blog/blog.py
@@ -5,15 +5,19 @@ class Blog:
         self.posts = []
 
     def __repr__(self):
-        return '{} by {} ({} posts)'.format(self.title, self.author, len(self.posts))
+        return '{} by {} ({} posts)'.format(self.title,
+                                            self.author,
+                                            len(self.posts),
+                                            's' if len(self.posts) != 1 else '')
 
     def create_post(self, title, content):
-        # self.posts.append(Post(title, content))
         pass
+        # self.posts.append(Post(title, content))
 
     def json(self):
-        return {
-            'title': self.title,
-            'author': self.author,
-            'posts': [post.json() for post in self.posts],
-        }
+        pass
+        # return {
+        #     'title': self.title,
+        #     'author': self.author,
+        #     'posts': [post.json() for post in self.posts],
+        # }
